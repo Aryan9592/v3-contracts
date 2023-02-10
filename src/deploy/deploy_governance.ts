c1a44ba10e176ee4f03607c1b5451b16cb40e541
gab <gabriele.rigo@gmail.com>
2023-02-10 20:48:48 +0100

    chore: add bsc staking address to governance deploy script

--- src/deploy/deploy_governance.ts
+++ src/deploy/deploy_governance.ts
@@ -15,8 +15,8 @@ const deploy: DeployFunction = async function (
   // arbitrum: 0xD495296510257DAdf0d74846a8307bf533a0fB48
   // optimism: 0xB844bDCC64a748fDC8c9Ee74FA4812E4BC28FD70
   // polygon: 0xC87d1B952303ae3A9218727692BAda6723662dad
-  // bsc: NA
-  const stakingProxy = {address: "0x6C4594aa0CBcb8315E88EFdb11675c09A7a5f444"};
+  // bsc: 0xa4a94cCACa8ccCdbCD442CF8eECa0cd98f69e99e
+  const stakingProxy = {address: "0x730dDf7b602dB822043e0409d8926440395e07fE"};
 
   const governanceFactory = await deploy("RigoblockGovernanceFactory", {
     from: deployer,
